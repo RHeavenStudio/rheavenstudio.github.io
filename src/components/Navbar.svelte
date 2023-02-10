eed8f14ac0c2b1f342a6f532de7f0dc7bbc26a38
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 17:21:17 -0500

    sexier blog posts

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Heaven Studio</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Editor</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contributing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tips</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+  
--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -1,10 +1,29 @@
 <script>
     export let data;
-  </script>
-  
-  <article>
+    import Navbar from "../../../components/Navbar.svelte";
+</script>
+
+<style>
+    	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+</style>
+
+<Navbar/>
+
+<article class="markdown-body">
     <h1>{ data.title }</h1>
     <p>Published: {data.date}</p>
-    <p>Author: {data.author}</p>
+    <h3>Author: {data.author}</h3>
     <svelte:component this={data.content} />
-  </article>
+</article>
