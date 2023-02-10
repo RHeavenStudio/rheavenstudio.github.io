4964a396c89dbbd67b6190ab21ce7722b8cf7cd6
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 16:46:38 -0500

    Testing out slugs

---
+++ src/routes/docs/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data;
+  </script>
+  
+  <article>
+    <h1>{ data.title }</h1>
+    <p>Published: {data.date}</p>
+    <p>Author: {data.author}</p>
+    <svelte:component this={data.content} />
+  </article>
