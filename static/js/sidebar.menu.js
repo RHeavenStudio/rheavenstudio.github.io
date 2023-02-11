7ac50de3d279851f872eda5349b9a9ce9bbb986c
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 23:59:58 -0500

    fixed sidebar

--- static/js/sidebar.menu.js
+++ static/js/sidebar.menu.js
@@ -1,37 +1,36 @@
+
 /*!
  * Sidebar menu for Bootstrap 4
  * Copyright Zdeněk Papučík
  * MIT License
- */
+*/
 (function($) {
 
-  // toggle sidebar menu
-  $('#sidebar-toggle').on('click', function() {
-    $('#wrapper').toggleClass('sidebar-toggle');
-  });
+	// toggle sidebar menu
+	$('#sidebar').on('click', function() {
+		$('#wrapper').toggleClass('toggle');
+	});
 
-  // list init
-  $('.list-item').each(function() {
-    $(this).parent().find('.link-arrow').addClass('up');
-    if ($(this).find('.link-current').length > 0) {
-        $(this).parent().find('.link-current.link-arrow').addClass('active down');
-        $(this).parent().find('.link-current').next('.list-hidden').show();
-    }
-  });
+	// init menu
+	$('ul.menu li').each(function() {
+		$(this).parent().find('li.parent a.employ').addClass('up');
+		if ($(this).children('li.parent a.current').length > 0 ) {
+			$(this).parent().find('ul.submenu').toggle();
+			$(this).parent().find('li.parent a.employ').addClass('active down');
+		}
+	});
 
-  // list open hidden
-  $('.list-link').on('click', function() {
-    $(this).parent().find('.link-arrow').toggleClass('active');
-    $(this).next('.list-hidden').slideToggle('fast');
-  });
+	// active menu
+	$('ul.menu li a').on('click', function() {
+		$(this).parent('li.parent').find('a.employ').toggleClass('active');
+		$(this).parent().find('ul.submenu').slideToggle('fast');
+		$(this).parent().find('ul.submenu ul.submenu').toggle();
+	});
 
-  // list transition arrow
-  $('.link-arrow').on('click', function() {
-    $(this).addClass('transition');
-    $(this).toggleClass('rotate');
-    if ($(this).parent().find('.link-arrow').hasClass('down')) {
-        $(this).toggleClass('rotate-revert');
-    }
-  });
+	// click transition
+	$('a.employ').on('click', function() {
+		$(this).addClass('transition');
+		$(this).toggleClass('rotate');
+	});
 
 }(jQuery));
